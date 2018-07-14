fc811d435529cce66234c0d82419f68b098e0c0d
Hue nguyen <hueahgase189@gmail.com>
2018-07-14 16:19:25 +0700

    change to jsonObjectRequest

--- app/src/main/java/vn/hrtech/myapplication/TestActivity.java
+++ app/src/main/java/vn/hrtech/myapplication/TestActivity.java
@@ -9,6 +9,8 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.JsonRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.gson.JsonArray;
@@ -35,9 +37,9 @@ public class TestActivity extends AppCompatActivity {
 
         String url = "https://swapi.co/api/planets/?page=2";
 
-        StringRequest registrationRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+        /*JsonRequest registrationRequest = new JsonRequest(Request.Method.GET, url, new Response.Listener<String>() {
             @Override
-            public void onResponse(String response) {
+            public void onResponse(JSONObject response) {
                 Log.d("RESPONSE", "|" + response + "|");
                 String jsonString = response;
                 JsonParser parser = new JsonParser();
@@ -55,8 +57,18 @@ public class TestActivity extends AppCompatActivity {
             public String getBodyContentType() {
                 return "application/json; charset=utf-8";
             }
-        };
-
+        };*/
+        JsonObjectRequest registrationRequest = new JsonObjectRequest(Request.Method.GET, url, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.d("JSON", "onResponse: " + response.toString());
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("JSON", "onErrorResponse: " + error.toString() );
+            }
+        });
         myQueue.add(registrationRequest);
     }
 
